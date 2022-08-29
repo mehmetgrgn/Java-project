a5dc29ae13a4ac085b9c79b1797131be17e3d746
byMett <58536831+byMett@users.noreply.github.com>
2022-08-29 16:54:49 +0300

    First Commit

--- JavaPatikasi/src/Giris/kdv_hesaplama.java
+++ JavaPatikasi/src/Giris/kdv_hesaplama.java
@@ -16,5 +16,7 @@ public class kdv_hesaplama {
         System.out.println("KDV Oranı : " + kdvOran);
         System.out.println("KDV Tutarı : " + kdvTutar);
         System.out.println("KDV'li Tutarı : " + kdvliTutar);
+        System.out.println("KDV'li Tutarı : " + kdvliTutar);
+
 }
 }
