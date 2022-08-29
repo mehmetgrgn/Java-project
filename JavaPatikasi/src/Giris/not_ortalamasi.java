0a85e770e45fb554915e2e701f00de1ddef679fd
byMett <58536831+byMett@users.noreply.github.com>
2022-08-29 16:49:25 +0300

    Initial commit

---
+++ JavaPatikasi/src/Giris/not_ortalamasi.java
@@ -0,0 +1,27 @@
+package Giris;
+import java.util.Scanner;
+
+public class not_ortalamasi {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        System.out.println("Matematik Notunuzu Giriniz : ");
+        mat = input.nextInt();
+        System.out.println("Fizik Notunuzu Giriniz : ");
+        fizik = input.nextInt();
+        System.out.println("Kimya Notunuzu Giriniz : ");
+        kimya = input.nextInt();
+        System.out.println("Türkçe Notunuzu Giriniz : ");
+        turkce = input.nextInt();
+        System.out.println("Tarih Notunuzu Giriniz : ");
+        tarih = input.nextInt();
+        System.out.println("Müzik Notunuzu Giriniz : ");
+        muzik = input.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double ortalama = toplam / 6;
+
+        System.out.println(ortalama);
+    }
+}
