1906fb85fac538c14ee5064c3601df9bb145b4ef
byMett <58536831+byMett@users.noreply.github.com>
2022-10-30 21:20:11 +0300

    First Commit

---
+++ JavaPatikasi/src/Giris/Metotlar/Desene_Gore_Metot_Olusturma.java
@@ -0,0 +1,44 @@
+package Metotlar;
+
+import java.util.Scanner;
+
+public class Desene_Gore_Metot_Olusturma {
+    static void  desen(int N, int tempNumber, int result) {
+
+        if (tempNumber > 0) {
+            System.out.print(result + " ");
+            result -= 5;
+
+            if (result <= 0) {
+                tempNumber = result;
+            }
+
+        } else if (tempNumber <= 0) {
+            System.out.print(result + " ");
+            result += 5;
+
+            if(result == N) {
+                System.out.print(result + " ");
+                return;
+            }
+
+        }
+
+        desen(N,tempNumber, result);
+
+    }
+    public static void main(String[] args) {
+        int result, tempNumber;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int N = input.nextInt();
+
+        result = N;
+        tempNumber = N;
+
+        desen(N, result,tempNumber);
+
+
+    }
+}
